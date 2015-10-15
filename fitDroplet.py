0bc1d4e412ddfc6422df2491511364dc7b4cc00c
Adam Chatterley <achatt@pslogin03.slac.stanford.edu>
2015-10-15 07:43:36 -0700

    added dummy fitting

--- fitDroplet.py
+++ fitDroplet.py
@@ -250,7 +250,7 @@ def dofitting(img, rawMask):
 #     plt.show()
 #     #cartesianFit(drop['a'], drop['b'], centre[1], centre[0], drop['phi'], newIm, newMask)
     
-#plt.hist(means)
-#plt.show()
-
+def dummyFit(img, rawMask):
+        drop = {'a': 0, 'b':0, 'x0':0, 'y0':0, 'phi':0, 'theta':0, 'peakPos':0, 'peakH':0, 'ovalFunc':0, 'reducedRes':0}
+    return {'orig':img[0:1024,0:1024].copy(order='C') , 'fit'::img[0:1024,0:1024].copy(order='C'), 'drop':drop}
 
